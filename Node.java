305b3312ea433751cb7bc70d02d4065af14e6b79
AnnabelZhang1 <azhang22@stuy.edu>
2021-01-06 19:21:53 -0500

    started setData and getData

--- Node.java
+++ Node.java
@@ -1,12 +1,42 @@
 public class Node{
 
-  public Node(String value){
-
+  public Node(String value, Node n, Node p){
+    data = value;
+    next = n;
+    prev = p;
   }
 
   private String data;
   private Node next, prev;
 
+//data
+  public String getData(){
+    return data;
+  }
+
+  public String setData(String value){
+    String old = data;
+    data = value;
+    return old;
+  }
+
+//next
+  public getNext(next){
+    return next;
+  }
+
+  public setNext(next){
+  
+  }
+
+//prev
+  public getPrev(prev){
+    return Node;
+  }
+
+  public setPrev(prev){
+
+  }
 
 }
-//write get/set methods for al three instance variables(data, next, prev)
+//write get/set methods for all three instance variables(data, next, prev)
